3b5ba4b493407d7475254c4d2d009026edf39266
Aditya Ghosh <104888138+AdityaGCSE@users.noreply.github.com>
2022-09-12 03:36:34 +0530

    Update Search.js

--- frontend/src/component/Product/Search.js
+++ frontend/src/component/Product/Search.js
@@ -16,11 +16,11 @@ const Search = ({ history }) => {
 
   return (
     <Fragment>
-      <MetaData title="Search A Product -- ECOMMERCE" />
+      <MetaData title="GiftEasy: Search Gifts" />
       <form className="searchBox" onSubmit={searchSubmitHandler}>
         <input
           type="text"
-          placeholder="Search a Product ..."
+          placeholder="Search for Gifts ..."
           onChange={(e) => setKeyword(e.target.value)}
         />
         <input type="submit" value="Search" />
