5d571f2449f6e86c28c8dcef59f4fe68222a0c77
Aditya Ghosh <104888138+AdityaGCSE@users.noreply.github.com>
2022-09-12 03:28:47 +0530

    Update Header.js

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,42 +1,41 @@
-import React from "react";
-import { ReactNavbar } from "overlay-navbar";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import logo from "../../../images/logo.png";
+import {BsFillCartDashFill} from "react-icons/bs";
+import {BsPersonCircle} from "react-icons/bs";
+import {BsSearch} from "react-icons/bs";
 
-const options = {
-  burgerColorHover: "#eb4034",
-  logo,
-  logoWidth: "20vmax",
-  navColor1: "white",
-  logoHoverSize: "10px",
-  logoHoverColor: "#eb4034",
-  link1Text: "Home",
-  link2Text: "Products",
-  link3Text: "Contact",
-  link4Text: "About",
-  link1Url: "/",
-  link2Url: "/products",
-  link3Url: "/contact",
-  link4Url: "/about",
-  link1Size: "1.3vmax",
-  link1Color: "rgba(35, 35, 35,0.8)",
-  nav1justifyContent: "flex-end",
-  nav2justifyContent: "flex-end",
-  nav3justifyContent: "flex-start",
-  nav4justifyContent: "flex-start",
-  link1ColorHover: "#eb4034",
-  link1Margin: "1vmax",
-  profileIconUrl: "/login",
-  profileIconColor: "rgba(35, 35, 35,0.8)",
-  searchIconColor: "rgba(35, 35, 35,0.8)",
-  cartIconColor: "rgba(35, 35, 35,0.8)",
-  profileIconColorHover: "#eb4034",
-  searchIconColorHover: "#eb4034",
-  cartIconColorHover: "#eb4034",
-  cartIconMargin: "1vmax",
-};
-
-const Header = () => {
-  return <ReactNavbar {...options} />;
-};
+function Header() {
+  return (
+    <Navbar collapseOnSelect expand="lg" style={{backgroundColor:"#e66470"}} variant="dark">
+      <Container>
+            <Navbar.Brand href="/">
+            <img
+              src={logo}
+              width="180"
+              height="90"
+              className="d-inline-block align-top"
+              alt="GifteEasy"
+            />
+          </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="products">Products</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="Search">Search <BsSearch/> </Nav.Link>
+            <Nav.Link href="cart">Cart <BsFillCartDashFill/>  </Nav.Link>
+            <Nav.Link href="login">Account <BsPersonCircle/> </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
 export default Header;
