5fcf3212aefa3445d4b7b5d348d429cecc225dc0
Aditya Ghosh <104888138+AdityaGCSE@users.noreply.github.com>
2022-09-12 03:33:24 +0530

    Update About.js

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -3,14 +3,14 @@ import "./aboutSection.css";
 import { Button, Typography, Avatar } from "@material-ui/core";
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import InstagramIcon from "@material-ui/icons/Instagram";
+import suddhasil from "../../../images/suddhasil.png";
 const About = () => {
   const visitInstagram = () => {
-    window.location = "https://instagram.com/meabhisingh";
+    window.location = "https://instagram.com/";
   };
   return (
     <div className="aboutSection">
       <div></div>
-      <div className="aboutSectionGradient"></div>
       <div className="aboutSectionContainer">
         <Typography component="h1">About Us</Typography>
 
@@ -18,28 +18,27 @@ const About = () => {
           <div>
             <Avatar
               style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
-              src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/jpyibarlaxawvcvqjv5b.png"
+              src={suddhasil}
               alt="Founder"
             />
-            <Typography>Abhishek Singh</Typography>
+            <Typography>Suddhasil Chatterjee</Typography>
             <Button onClick={visitInstagram} color="primary">
               Visit Instagram
             </Button>
             <span>
-              This is a sample wesbite made by @meabhisingh. Only with the
-              purpose to teach MERN Stack on the channel 6 Pack Programmer
+              Gift Item Seller
             </span>
           </div>
           <div className="aboutSectionContainer2">
             <Typography component="h2">Our Brands</Typography>
             <a
-              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
+              href="https://www.youtube.com/channel/"
               target="blank"
             >
               <YouTubeIcon className="youtubeSvgIcon" />
             </a>
 
-            <a href="https://instagram.com/meabhisingh" target="blank">
+            <a href="https://instagram.com/" target="blank">
               <InstagramIcon className="instagramSvgIcon" />
             </a>
           </div>
