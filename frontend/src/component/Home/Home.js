86f659c308c6f83b5eefbe5dd538347694f2a9c3
Aditya Ghosh <104888138+AdityaGCSE@users.noreply.github.com>
2022-09-12 13:48:15 +0530

    Update Home.js

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -31,7 +31,7 @@ const Home = () => {
 
           <div className="banner">
             <p>Welcome to GiftEasy</p>
-            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+            <h1>FIND AMAZING GIFTS BELOW</h1>
 
             <a href="#container">
               <button>
