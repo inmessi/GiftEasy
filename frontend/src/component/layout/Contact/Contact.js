68143d01408a0a681508c864be92b2a2dd92b349
Aditya Ghosh <104888138+AdityaGCSE@users.noreply.github.com>
2022-09-12 22:22:49 +0530

    Update Contact.js

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -6,8 +6,11 @@ const Contact = () => {
   return (
     <div className="contactContainer">
       <a className="mailBtn" href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJNqLMmkVvSMwrdnQwlvXVpzFdpDXfFlKzbxLvxxtLJSDHKrwfMLwpxvdphxvCkddWMhmjV">
-        <Button>Contact: csuddhasil@gmail.com</Button>
+        <Button>Email address: csuddhasil@gmail.com</Button>
       </a>
+      <div className="contactno">
+        <h1>Contact Number: 9874835785</h1>
+      </div>
     </div>
   );
 };
