2f401991ba91038c8affc909c75d6f1960e9badd
Aditya Ghosh <104888138+AdityaGCSE@users.noreply.github.com>
2022-09-13 13:58:55 +0530

    Update Contact.js

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -1,19 +1,13 @@
 import React from "react";
 import "./Contact.css";
 import { Button } from "@material-ui/core";
-
 const Contact = () => {
   return (
     <div className="contactContainer">
-    <abbr title="Click here to mail Suddhasil Chatterjee" className = 'abbr'>
       <a className="mailBtn" href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJNqLMmkVvSMwrdnQwlvXVpzFdpDXfFlKzbxLvxxtLJSDHKrwfMLwpxvdphxvCkddWMhmjV">
-
-        <Button>Email address: csuddhasil@gmail.com</Button><br/>
+        <Button>Email address: csuddhasil@gmail.com</Button>
       </a>
-
-    </abbr>
     </div>
   );
 };
-
 export default Contact;
