7f1b11fa3a3d3cfd8208973a40f229687b19a8b7
Aditya Ghosh <104888138+AdityaGCSE@users.noreply.github.com>
2022-09-13 18:44:35 +0530

    Update Header.js

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -26,14 +26,14 @@ function Header() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="products">Products</Nav.Link>
-            <Nav.Link href="contact">Contact</Nav.Link>
-            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="/products">Products</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="Search">Search <BsSearch/> </Nav.Link>
-            <Nav.Link href="cart">Cart <BsFillCartDashFill/>  </Nav.Link>
-            <Nav.Link href="login">Account {!isAuthenticated && <BsPersonCircle/>} </Nav.Link>
+            <Nav.Link href="/Search">Search <BsSearch/> </Nav.Link>
+            <Nav.Link href="/cart">Cart <BsFillCartDashFill/>  </Nav.Link>
+            <Nav.Link href="/login">Account {!isAuthenticated && <BsPersonCircle/>} </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
